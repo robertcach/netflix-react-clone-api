03318c0c05ff112503b5b5261591d6a03c7d03be
Robert Cabrera <cabrera.rch@gmail.com>
2022-04-28 16:43:31 +0200

    commit

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.use((error, req, res, next) => {
   res.status(error.status).json(data);
 });
 
-const port = Number(process.env.PORT || 3001);
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
   console.log(`Ready! Listen on port ${port}`);
